24878b57e2048cc06f17f1c4b15f5a41dffe4fd2
Eduardo <eduardozwenger@hotmail.com>
2022-10-13 18:09:52 -0300

    cambio de prueba

--- src/components/WhoMakes/Photoone.jsx
+++ src/components/WhoMakes/Photoone.jsx
@@ -1,37 +1,19 @@
-import { VscTelescope} from 'react-icons/vsc' ;
+
+import '../WhoMakes/Photoone.css';
+import Vector from "../../imagenes/Vector.jpg"
 
 const Photoone = () => {
   return (
-    <figure style={{
-        height:"15.625rem",
-        width:"13.125rem",
-        position:"relative",
-        margin:"0rem",
-        display:"block",
-      }}>
-        < VscTelescope size={"4rem"} / > 
-        <div>
-          <p style={{
-            fontSize:"25px",
-            position:"relative",
-            fontStyle:"normal",
-            fontWeight:"43.75rem",
-            lineHeight:"1.5625rem",
-            margin:"0rem",
-            fontFamily:"Cormorant Garamond , serif",
-          }}>Descubrir nuevos <br></br> sabores y personas</p>
-        </div>
-        <div >
-            <p style={{
-              position:"relative",
-              marginTop:"0.75rem",
-              fontSize:"1rem",
-              fontStyle:"normal",
-              lineHeight:"1.5rem",
-              color:"#1F1F1F",
-              fontFamily: "Noto Sans Balinese sans-serif",
-            }}>Descubre nuevos gustos, sabores, snacks y recetas para hacer comidas maravillosas. También averigüe de dónde viene su comida y quién la hace.</p>
-        </div>
+    <figure>
+      <div className='telescopio'>
+          <img  src={Vector} alt="Vector" />
+      </div>
+      <div>
+          <p className='uno'>Descubrir nuevos <br></br> sabores y personas</p>
+      </div>
+      <div >
+          <p className='dos'>Descubre nuevos gustos, sabores, snacks y recetas para hacer comidas maravillosas. También averigüe de dónde viene su comida y quién la hace.</p>
+      </div>
       </figure>
   )
 }
--- src/components/WhyChoose/Targetfour.jsx
+++ src/components/WhyChoose/Targetfour.jsx
@@ -1,20 +1,11 @@
 
-
+import '../WhyChoose/Targetfour.css'
 import { FiStar } from "react-icons/fi" ;
 
 const Targetfour = () => {
   return (
     <div>     
-        <p style={{
-            position:"absolute", 
-            fontSize:"17px",
-            lineHeight:"24px",
-            marginTop:"225px",
-          marginLeft:"10px",
-        }}> <FiStar style={{
-          fontSize:"20px",
-          color:"#570266",
-        }}/>Porque apoya empleos locales.</p>
+        <p className="cuatro4"> <FiStar className="estrella4"/>Porque apoya empleos locales.</p>
     </div>
   )
 }
